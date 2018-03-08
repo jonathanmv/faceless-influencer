fa20c1eda44607a0e4618878fa50c9751ab3c465
Jonathan Morales Vélez <jonathan@themidgame.com>
2018-03-08 22:55:42 +0100

    Added source to create video command

--- mediaHelper.js
+++ mediaHelper.js
@@ -17,6 +17,11 @@ const takeScreenshot = url => {
   return screenshot(url, fileName).then(() => fileName)
 }
 
+/*
+create a video from single image and audio
+https://superuser.com/questions/1041816/combine-one-image-one-audio-file-to-make-one-video-using-ffmpeg
+ffmpeg -loop 1 -i shot.png -i speech.mp3 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest out.mp4
+*/
 const createVideo = (image, audio) => {
   const fileName = './videos/' + new Date().getTime() + '.mp4'
   const command = [
