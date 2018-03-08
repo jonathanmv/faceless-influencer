3665807e35ee7be342ae3f01f5da0efc920e4c6c
Jonathan Morales Vélez <jonathan@themidgame.com>
2018-03-08 22:47:53 +0100

    added username and post params

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-const _ = require('lodash')
-
 const awsHelper = require('./awsHelper')
 const mediumHelper = require('./mediumHelper')
 const mediaHelper = require('./mediaHelper')
 
 const log = console.log.bind(console)
 
-const run = async (username, postNumber) => {
+const makeVideo = async (username, postNumber) => {
+  return console.log(username, postNumber)
   const postIds = await mediumHelper.latestPostsIds(username)
   const postId = postIds[postNumber - 1]
   if (!postId) {
-    console.log(`Couldn't find post #${postNumber}`)
+    log(`Couldn't find post #${postNumber}`)
     return
   }
-  console.log('Getting user post analysis')
+  log('Getting user post analysis')
   const analysis = await mediumHelper.getUserPostAnalysis(username, postId)
-  console.log(analysis)
-  console.log('Saving speech ...')
+  log(analysis)
+  log('Saving speech ...')
   const speechPath = await awsHelper.saveSpeechLocally(analysis)
-  console.log('Saving screenshot ...')
+  log('Saving screenshot ...')
   const screenshotPath = await mediumHelper.saveUserPostScreenshotLocally(username, postId)
-  console.log(`Creating video from ${screenshotPath} and ${speechPath}`)
+  log(`Creating video from ${screenshotPath} and ${speechPath}`)
   return mediaHelper.createVideo(screenshotPath, speechPath)
 }
 
-const usernames = [
-  'julsimon',
-  'adhorn',
-  'jonathanmv'
-]
+const run = ({ username = 'jonathanmv', post = 1 }) => {
+  return makeVideo(username, post)
+    .catch(log)
+}
 
-run(usernames[2], 1)
-.then(log)
-.catch(log)
+run(process.env)
