eee3ef07a7423c55faf6eecdfdf095ccff1dcfa5
Jonathan Morales Vélez <jonathan@themidgame.com>
2018-03-08 16:25:13 +0100

    Build a text description of a post

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const axios = require('axios')
 const _ = require('lodash')
+const plural = require('plural')
+
+const comprehendEntitiesResponse = require('./samples/comprehendEntitiesResponse.json')
 
 const username = 'julsimon'
-const streamItemsPath = ''
+const postIds = [
+  '46345ccec0f4',
+  'da09f30cca38',
+  '8d14fce03b9',
+  'ba8eededd737',
+  '4bce4e56ef55',
+  'f6ee46182f80',
+  'e371cf94ddb5',
+  'cedbfc8c93d8',
+  '5596f38931b3',
+  'da819d8a4887'
+]
+
 
 const asJson = url => `${url}?format=json`
-const latestPostsUrl = username => asJson(`https://medium.com/@${username}/latest`)
+const userUrl = username => `https://medium.com/@${username}`
+const latestPostsUrl = username => asJson(`${userUrl(username)}/latest`)
+const userPostUrl = (username, postId) => asJson(`${userUrl(username)}/${postId}`)
+
 
 const postsIdsFromResponse = response => {
   const items = _.get(response, 'payload.streamItems', [])
@@ -30,5 +48,83 @@ const latestPostsIds = async username => {
   return postsIdsFromResponse(data)
 }
 
+const DEFAULT_TYPES = [
+  1, // p
+  3, // h3,
+  13, // h4,
+  // 4, // figcaption for images
+  // 6, // blockquote
+]
+const textInPostFromResponse = (response, types = DEFAULT_TYPES) => {
+  const paragraphs = _.get(response, 'payload.value.content.bodyModel.paragraphs', [])
+  const filtered = paragraphs.filter(({ type }) => types.includes(type))
+  return filtered.map(({text}) => text)
+}
+
+const textInPostId = async (username, postId) => {
+  const url = userPostUrl(username, postId)
+  const data = await getJsonData(url)
+  return textInPostFromResponse(data)
+}
+
+// COMMERCIAL_ITEM to comercial item
+const cleanName = name => _.words(name.toLowerCase()).join(' ')
+
+const toNameCount = object => Object.keys(object).map(name => ({ name, count: object[name] }))
+const sortByCountDesc = objects => _.sortBy(objects, 'count').reverse()
+const toSortedCountDesc = object => sortByCountDesc(toNameCount(object))
+
+const getStatsFromComprehendResponse = ({ Entities }) => {
+  const entityTypeCounts = _.countBy(_.uniqBy(Entities, 'Text'), 'Type')
+  const sortedTypesCount = toSortedCountDesc(entityTypeCounts)
+  const nonQuantityEntities = Entities.filter(({ Type }) => Type != 'QUANTITY')
+  const entityNameCounts = _.countBy(nonQuantityEntities, 'Text')
+  const sortedEntityNamesCount = toSortedCountDesc(entityNameCounts)
+  return { sortedTypesCount, sortedEntityNamesCount }
+}
+
+const describeEntityCounts = counts => {
+  const top = counts[0]
+  const intro = `We find a total of ${counts.length} entities mentioned. `
+  const topCount = `Mainly "${top.name}" which appears ${top.count} times, followed by`
+  const topCounts = counts.slice(1, 4).map(({name, count}) => `"${name}" mentioned ${count} times`).join(', ')
+  const rest = counts.slice(4, 7).map(({name}) => name).join(', ')
+  const others = `. Some other mentions include ${rest} plus many others`
+  return intro + topCount + topCounts + others
+}
+
+const describeTypesCounts = counts => {
+  const intro = `Regarding the types of entities, it includes `
+  const other = counts.find(({name}) => name = 'OTHER')
+  const noOther = counts.filter(({name}) => name != 'OTHER')
+  const body = noOther.map(({name, count}) => `${count} ${plural(cleanName(name), count)}`).join(', ')
+  const closing = `, and also some other ${other.count} things.`
+  return intro + body + closing
+}
+
+const describePostTexts = texts => {
+  const postTitle = texts[0]
+  const postIntro = texts.slice(1, 4).join('.\n')
+  const intro = `The post is titled "${postTitle}" and it reads as it follows:\n`
+  return intro + postIntro
+}
+
 const log = console.log.bind(console)
-latestPostsIds(username).then(log).catch(log)
+const run = async (username, postId) => {
+  // latestPostsIds(username)
+  // .then(log).catch(log)
+  const textsInPost = await textInPostId(username, postId)
+  const postTextDescription = describePostTexts(textsInPost)
+  const stats = getStatsFromComprehendResponse(comprehendEntitiesResponse)
+  const descriptions = [
+    describePostTexts(textsInPost),
+    `Ok, now let's analyze it`,
+    describeEntityCounts(stats.sortedEntityNamesCount),
+    describeTypesCounts(stats.sortedTypesCount)
+  ]
+  const toRead = descriptions.join('.\n')
+  console.log(toRead)
+}
+
+run(username, postIds[3])
+.catch(log)
