00510a28c53ed7dfb6f44ed9281b7b442b5f7ecd
Jonathan Morales Vélez <jonathan@themidgame.com>
2018-03-13 16:14:57 +0100

    Added reference to article describing effects with ffmpeg

--- mediaHelper.js
+++ mediaHelper.js
@@ -21,6 +21,9 @@ const takeScreenshot = url => {
 create a video from single image and audio
 https://superuser.com/questions/1041816/combine-one-image-one-audio-file-to-make-one-video-using-ffmpeg
 ffmpeg -loop 1 -i shot.png -i speech.mp3 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest out.mp4
+
+Effects
+https://el-tramo.be/blog/ken-burns-ffmpeg/
 */
 const createVideo = (image, audio) => {
   const fileName = './videos/' + new Date().getTime() + '.mp4'
